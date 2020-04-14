3a1ee21dfe3ea714dafde4cc4509e2093951cc44
Fx <admin@52tutu.com.cn>
2020-04-14 15:16:36 +0800

    解决json格式自动解密@RequestParam参数无值

--- encrypt-core/src/main/java/com/cxytiandi/encrypt/core/EncryptionReqestWrapper.java
+++ encrypt-core/src/main/java/com/cxytiandi/encrypt/core/EncryptionReqestWrapper.java
@@ -8,12 +8,15 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.cxytiandi.encrypt.util.StreamUtils;
 
 public class EncryptionReqestWrapper extends HttpServletRequestWrapper  {
-	
-	private byte[] requestBody = new byte[0];
-	
+
+	private byte[] requestBody;
+    private JSONObject jsonBody;
+
 	public EncryptionReqestWrapper(HttpServletRequest request) {
 		super(request);
 		try {
@@ -22,7 +25,29 @@ public class EncryptionReqestWrapper extends HttpServletRequestWrapper  {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
+    @Override
+    public String getParameter(String name) {
+        if (null != this.jsonBody) {
+            Object obj = this.jsonBody.getObj(name);
+            if (null != obj) {
+                return String.valueOf(obj);
+            }
+        }
+        return super.getParameter(name);
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        if (null != this.jsonBody) {
+            Object obj = this.jsonBody.getObj(name);
+            if (null != obj) {
+                return new String[]{String.valueOf(obj)};
+            }
+        }
+        return super.getParameterValues(name);
+    }
+
 	@Override
 	public ServletInputStream getInputStream() throws IOException {
         final ByteArrayInputStream bais = new ByteArrayInputStream(requestBody);
@@ -52,8 +77,12 @@ public class EncryptionReqestWrapper extends HttpServletRequestWrapper  {
 	public String getRequestData() {
 		return new String(requestBody);
 	}
-	
-	public void setRequestData(String requestData) {
-		this.requestBody = requestData.getBytes();
-	}
+
+    public void setRequestData(String requestData) {
+        this.requestBody = requestData.getBytes();
+        // json格式解析
+        if (null != requestData && requestData.startsWith("{")) {
+            this.jsonBody = JSONUtil.parseObj(requestData);
+        }
+    }
 }
